d6a780a9b93418989976a94d3407eed6e0855bf2
Ruddy <rudraparbat714@gmail.com>
2025-08-28 15:30:37 +0530

    update allowed hosts

--- travelers/settings.py
+++ travelers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-jn8oa(gfe^6gl(cox-#2_3&!93&3f@9!^f8+3jkrymcim$3-u)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
